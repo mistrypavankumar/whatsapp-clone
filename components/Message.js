9d58d7753c8d7ce0de8365fe16113ba9db0f5a93
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-06-03 00:50:34 +0530

    added chat screen

--- components/Message.js
+++ components/Message.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Message = ({ user, message }) => {
   return (
     <Container>
-      <p>{message}</p>
+      <p>{message.message}</p>
     </Container>
   );
 };
